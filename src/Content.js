378418f87781aa28696480962e7afb72b27b2514
suncrownfisher <suncrownfisher@gmail.com>
2023-03-28 18:22:25 +0800

    first commit

---
+++ src/Content.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react';
+import { FaTrashAlt } from "react-icons/fa";
+
+
+const Content = () => {
+    const [items, setItems] = useState([
+      {
+        id: 1,
+        checked: true,
+        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Item 3"
+      }
+    ]);
+    
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" 
+              key={item.id}>
+            <input 
+              type="checkbox" 
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt 
+              role="button" 
+              tabIndex="0"
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default Content
